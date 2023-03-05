53df0575059a77b24954bfb4f459cdea97a4e00b
syn <1599386309@qq.com>
2023-03-05 23:47:23 +0800

    no message

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,18 +1,3 @@
 module.exports = {
-  "lint-staged": {
-    "src/**/*.{tsx,ts,js,jsx}": [
-      "prettier --write",
-      "eslint --fix",
-      "git add"
-    ],
-    "src/**/*.less": [
-      "stylelint --syntax less --fix",
-      "git add"
-    ]
-  },
-  "husky": {
-    "hooks": {
-      "pre-commit": "lint-staged"
-    }
-  }
-}
+  extends: ['@commitlint/config-conventional'],
+};
